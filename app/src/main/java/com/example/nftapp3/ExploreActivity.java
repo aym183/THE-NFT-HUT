a6af710cccfe1c4b64df0d5d31b8c72d5daa3d86
aym183 <ayman.ali1302@gmail.com>
2022-04-14 02:52:45 +0100

    Added base explore page design layout

--- app/src/main/java/com/example/nftapp3/ExploreActivity.java
+++ app/src/main/java/com/example/nftapp3/ExploreActivity.java
@@ -2,10 +2,14 @@ package com.example.nftapp3;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,6 +17,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 
 public class ExploreActivity extends AppCompatActivity {
@@ -24,6 +41,105 @@ public class ExploreActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_explore);
+
+
+        OkHttpClient client = new OkHttpClient();
+
+        //String url = "https://opensea13.p.rapidapi.com/assets?collection_slug=cryptopunks&order_direction=desc&limit=20&include_orders=false";
+        Request request = new Request.Builder()
+                .url("https://opensea13.p.rapidapi.com/assets?collection_slug=cryptopunks&order_direction=desc&limit=13&include_orders=false")
+                .get()
+                .addHeader("X-RapidAPI-Host", "opensea13.p.rapidapi.com")
+                .addHeader("X-RapidAPI-Key", "3b11ee2336msh5791c275fc5dbc6p11fa37jsn3cafb1ed4e82")
+                .build();
+
+        // TextView imageView = findViewById(R.id.textView5);
+//        String myResponse = "API";
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if(response.isSuccessful()){
+                    String myResponse = response.body().string();
+
+//                    String jsonData = myResponse.body().string();
+                    try {
+                        JSONObject json = new JSONObject(myResponse);
+
+                        JSONArray arr = json.getJSONArray("assets");
+                        int sale_details = 0;
+
+                        for (int i = 3; i < arr.length(); i++)
+                        {
+
+                            String post_id = arr.getJSONObject(i).getString("image_preview_url");
+                            String post_name = arr.getJSONObject(i).getString("name");
+                            String sale_name = arr.getJSONObject(i).getString("last_sale");
+
+
+                            if(sale_name.length() == 4){
+                                ;
+                            }
+                            else{
+                                JSONObject real_sale_name = arr.getJSONObject(i).getJSONObject("last_sale");
+//                                JSONObject json2 = new JSONObject(sale_name);
+//                                JSONArray arr2 = json.getJSONArray("payment_token");
+                                //String sale_price = arr.getJSONObject(i).getString("eth_price");
+                                sale_details += real_sale_name.getJSONObject("payment_token").getInt("eth_price");
+//                                Log.d("SUCCESS", String.valueOf(real_sale_name.getJSONObject("payment_token").getString("eth_price")));
+                                Log.d("SUCCESS2", String.valueOf(sale_details));
+                            }
+
+//                            titles2[i-3] = post_name;
+//                            imageDetails2[i-3] = post_id;
+
+//                            Log.d("DATAbyme"+i, post_id);
+//                            Log.d("DATAbyme"+i, post_name);
+//                            Log.d("DATAbyme"+i, "--------");
+
+                        }
+                        //setTitles2(copy_titles2);
+//                        Log.d("titles", Arrays.deepToString(titles2));
+//                        Log.d("Details", Arrays.deepToString(imageDetails2));
+                        TextView sales = findViewById(R.id.saleDetails);
+                        sales.setText("TOTAL VALUE:      " + sale_details);
+
+
+
+
+
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+//                    runOnUiThread(new Runnable() {
+//
+//                        @Override
+//                        public void run() {
+//
+////                            GridAdapter adapter =  new GridAdapter(HomeActivity.this, titles2, imageDetails2);
+////                            gridView.setAdapter(adapter);
+////                            gridView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+//                               // @Override
+////                                public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+////                                    //Toast.makeText(getApplicationContext(), "You clicked " + titles2[position],
+////                                            Toast.LENGTH_SHORT).show();
+////                                }
+//                            });
+//
+//                        }
+
+
+                }
+
+            }
+        });
+
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setSelectedItemId(R.id.explore);
