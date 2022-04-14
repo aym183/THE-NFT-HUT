46a8b29a59fecbc4edcffbc482bdc7424e0747ed
aym183 <ayman.ali1302@gmail.com>
2022-04-14 23:48:48 +0100

    Added Most Viewed row to explore page

--- app/src/main/java/com/example/nftapp3/ExploreActivity.java
+++ app/src/main/java/com/example/nftapp3/ExploreActivity.java
@@ -1,23 +1,14 @@
 package com.example.nftapp3;
 
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -38,8 +29,11 @@ import okhttp3.Response;
 public class ExploreActivity extends AppCompatActivity {
 
     ImageView ivResult;
-    int[] imageViews = {R.id.nft1, R.id.nft2, R.id.nft3, R.id.nft4, R.id.nft5};
-    int[] textViews = {R.id.nft1Text, R.id.nft2Text, R.id.nft3Text, R.id.nft4Text, R.id.nft5Text};
+    int[][] imageViews = {{R.id.nft1, R.id.nft2, R.id.nft3, R.id.nft4, R.id.nft5},
+            {R.id.second_nft1, R.id.second_nft2, R.id.second_nft3, R.id.second_nft4, R.id.second_nft5}};
+
+    int[][] textViews = {{R.id.nft1Text, R.id.nft2Text, R.id.nft3Text, R.id.nft4Text, R.id.nft5Text},
+            {R.id.second_nft1Text, R.id.second_nft2Text, R.id.second_nft3Text, R.id.second_nft4Text, R.id.second_nft5Text}};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -66,11 +60,12 @@ public class ExploreActivity extends AppCompatActivity {
         String most_viewedurl = "https://opensea13.p.rapidapi.com/assets?owner=0x276CD56089E7576Fb80d39a763aA0d213B98E948&order_direction=desc&limit=5&include_orders=false";
         String hot_new_itemsurl = "https://opensea13.p.rapidapi.com/assets?owner=0xE21DC18513e3e68a52F9fcDaCfD56948d43a11c6&order_direction=desc&limit=5&include_orders=false";
 
-        String[] urlLists = {recently_addedurl, most_viewedurl, hot_new_itemsurl};
+        String[] urlLists = {recently_addedurl, most_viewedurl};
         for(int i = 0; i<urlLists.length; i++){
 
+            int index_position = i;
             Request request = new Request.Builder()
-                    .url(urlLists[0])
+                    .url(urlLists[i])
                     .get()
                     .addHeader("X-RapidAPI-Host", "opensea13.p.rapidapi.com")
                     .addHeader("X-RapidAPI-Key", "3b11ee2336msh5791c275fc5dbc6p11fa37jsn3cafb1ed4e82")
@@ -96,34 +91,34 @@ public class ExploreActivity extends AppCompatActivity {
                             JSONArray arr = json.getJSONArray("assets");
 
 
-                            for (int i = 0; i < arr.length(); i++) {
+                            for (int j = 0; j < arr.length(); j++) {
 
-                                String post_id = arr.getJSONObject(i).getString("image_preview_url");
-                                String post_name = arr.getJSONObject(i).getString("name");
+                                String post_id = arr.getJSONObject(j).getString("image_preview_url");
+                                String post_name = arr.getJSONObject(j).getString("name");
 
 
-                                titles[i] = post_name;
-                                imageDetails[i] = post_id;
+                                titles[j] = post_name;
+                                imageDetails[j] = post_id;
 
 
                             }
 
                             Log.d("Details", Arrays.deepToString(imageDetails));
                             Log.d("Titles", Arrays.deepToString(titles));
-                            for(int i =0; i<imageDetails.length; i++) {
+                            for(int k =0; k<imageDetails.length; k++) {
 
-                                int imageView = imageViews[i];
-                                ivResult = findViewById(imageViews[i]);
-                                String textValue = titles[i];
+                                int imageView = imageViews[index_position][k];
+                                ivResult = findViewById(imageViews[index_position][k]);
+                                String textValue = titles[k];
                                 LoadImage newImage = new LoadImage(ivResult);
-                                newImage.execute(imageDetails[i]);
-                                int position = i;
+                                newImage.execute(imageDetails[k]);
+                                int position = k;
 
                                 runOnUiThread(new Runnable() {
                                     @Override
                                     public void run() {
 
-                                            TextView textView = findViewById(textViews[position]);
+                                            TextView textView = findViewById(textViews[index_position][position]);
                                             textView.setText(textValue);
 
                                     }
