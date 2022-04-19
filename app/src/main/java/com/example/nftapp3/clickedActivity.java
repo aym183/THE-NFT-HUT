d1a8e3c5b2b1b6228d8f79210abec5a28d5c0242
aym183 <ayman.ali1302@gmail.com>
2022-04-19 20:58:52 +0100

    Added back button to clickedActivity.java

--- app/src/main/java/com/example/nftapp3/clickedActivity.java
+++ app/src/main/java/com/example/nftapp3/clickedActivity.java
@@ -1,6 +1,8 @@
 package com.example.nftapp3;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,10 @@ public class clickedActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clicked);
+    }
+
+    public void backArrowEvent(View v){
+        startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+        overridePendingTransition(0, 0);
     }
 }
