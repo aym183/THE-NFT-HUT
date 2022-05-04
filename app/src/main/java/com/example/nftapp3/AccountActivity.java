07e703f10c4b07651fa178c70a4fd3d0371b182c
aym183 <ayman.ali1302@gmail.com>
2022-05-04 08:15:42 +0100

    Refactored AccountActivity.java

--- app/src/main/java/com/example/nftapp3/AccountActivity.java
+++ app/src/main/java/com/example/nftapp3/AccountActivity.java
@@ -33,27 +33,29 @@ import org.w3c.dom.Text;
 import java.util.Arrays;
 import java.util.HashMap;
 
+/**
+ * The AccountActivity class handles all the operations for the account page
+ */
 public class AccountActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
     int[] textViews = {R.id.firstName, R.id.lastName};
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_account);
-
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setSelectedItemId(R.id.account);
-
         FirebaseDatabase rootNode;
         DatabaseReference reference;
         String[] values = new String[3];
 
-        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://the-nft-hut-d2a38-default-rtdb.firebaseio.com/");
+        /* The username that is stored in Shared Preferences is retrieved
+        * and fetched to retrieve user details*/
 
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://the-nft-hut-d2a38-default-rtdb.firebaseio.com/");
         SharedPreferences sp = getApplicationContext().getSharedPreferences("Username", Context.MODE_PRIVATE);
         String value = sp.getString("username_value", "");
         TextView username = findViewById(R.id.username);
@@ -66,8 +68,14 @@ public class AccountActivity extends AppCompatActivity {
             int position = i;
 
             databaseReference.child("Users").child(value).child(text_data[i]).addValueEventListener(new ValueEventListener() {
+                /**
+                 * This method checks whether user has valid details and is the success case
+                 * @param snapshot The value/object containing the user details
+                 */
                 @Override
                 public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                    // Once snapshot exits, user details are displayed on the account page
                     if (snapshot.exists()) {
                         String data = snapshot.getValue().toString();
                         values[position] = data;
@@ -76,6 +84,10 @@ public class AccountActivity extends AppCompatActivity {
                     }
                 }
 
+                /**
+                 * This method handles failure case when checking for user details
+                 * @param error This displays the error
+                 */
                 @Override
                 public void onCancelled(@NonNull DatabaseError error) {
                     Log.d("VALUE FROM FIRE", "you dont work");
@@ -86,6 +98,11 @@ public class AccountActivity extends AppCompatActivity {
         }
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            /**
+             * This method handles click on the bottom navbar and where to redirect the user
+             * @param item This is the item in the navbar that the user clicks
+             * @return 'true' is returned once user clicks a particular item and is redirected correctly
+             */
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 Fragment fragment = null;
@@ -124,6 +141,10 @@ public class AccountActivity extends AppCompatActivity {
     }
 
 
+    /**
+     * This method handles LOGOUT event
+     * @param v This arguement represents the View that received the click event
+     */
     public void LogoutEvent(View v){
 
         startActivity(new Intent(getApplicationContext(), MainActivity.class));
@@ -131,6 +152,10 @@ public class AccountActivity extends AppCompatActivity {
 
     }
 
+    /**
+     * This method handles LEARN MORE event
+     * @param v This arguement represents the View that received the click event
+     */
     public void learnMoreEvent(View v){
         Uri webpage = Uri.parse("https://thenfthut.live");
         Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
